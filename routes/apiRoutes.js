5b0da1ab407dd18b7cce569e4a33fdb8d16a06ee
Tim Martin <timmartin13@gmail.com>
2021-02-24 09:06:26 -0600

    put and post routes look good

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,36 +2,46 @@ const db = require("../models");
 
 module.exports = function(app) {
     app.get("/api/workouts", function(req, res) {
-        db.Workout.find({}, (err, data) => {
-            if (err) {
-              console.log(err);
-            } else {
-              res.json(data);
-            }
+        // db.Workout.find({}, (err, data) => {
+        //     if (err) {
+        //       console.log(err);
+        //     } else {
+        //       res.json(data);
+        //     }
+        // });
+      db.Workout.find({})
+        .then(function(results) {
+          res.json(results);
+        })
+        .catch(function(err){
+          res.json(err);
         });
     });
   
     // PUT
     app.put("/api/workouts/:id", function(req, res) {
-      // 2; Add a join to include all of the Author's Posts here
-        console.log("------------------- put: api/workouts/:id -----------------------");
-    //   db.Author.findOne({
-    //     where: {
-    //       id: req.params.id
-    //     }
-    //   }).then(function(dbAuthor) {
-    //     res.json(dbAuthor);
-    //   });
+      db.Workout.updateOne({ _id: req.params.id },
+        { $push: 
+          { exercises: req.body }
+        })
+        .then(function(results) {
+          res.json(results);
+        })
+        .catch(function(err) {
+          res.json(err);
+        }); 
     });
   
     app.post("/api/workouts", function(req, res) {
-      console.log("------------------- post: api/workouts -----------------------");
-      console.log(req.body);
-      db.Workout.create(req.body).then(function(dbWorkout) {
-        
-        console.log(dbWorkout);
-        res.json(dbWorkout);
-      });
+      // console.log("------------------- post: api/workouts -----------------------");
+      // console.log(req.body);
+      db.Workout.create(req.body)
+        .then(function(dbWorkout) {
+          res.json(dbWorkout);
+        })
+        .catch(function(err) {
+          res.json(err);
+        });
     });
   
     app.get("/api/workouts/range", function(req, res) {
